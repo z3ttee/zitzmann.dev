7a821e3d95996dd77b5739911979faee316f6d9b
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2026-06-07 17:36:49 +0200

    added consts for imprint

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,7 @@ export const CURRENT_EMPLOYER_LINK = "https://finanzfluss.de";
 
 export const SOCIAL_LINKEDIN_LINK = "https://linkedin.com/in/cedric-zitzmann";
 export const SOCIAL_GITHUB_LINK = "https://github.com/z3ttee";
-export const SOCIAL_EMAIL_LINK = "mailto:cedric.zitzmann@gmail.com";
+export const SOCIAL_EMAIL = "cedric.zitzmann@gmail.com";
+export const SOCIAL_EMAIL_LINK = `mailto:${SOCIAL_EMAIL}`;
+
+export const LEGAL_SOURCE_LINK = "https://www.e-recht24.de/";
