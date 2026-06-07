c607083863ebd13a0f14cf9be80d20c82d36c58a
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2026-06-07 16:55:19 +0200

    use built-in handler instead

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
 import { paraglideMiddleware } from "./paraglide/server";
-
-const handler = createStartHandler(defaultStreamHandler);
+import handler from "@tanstack/react-start/server-entry";
 
 export default {
   fetch(req: Request) {
-    return paraglideMiddleware(req, () => handler(req));
+    return paraglideMiddleware(req, () => handler.fetch(req));
   },
 };
